1499fda54eae11a71cab713416cdd35f7107d3ec
Willeboy175 <william.strandberg@elev.ga.lbs.se>
2022-12-16 10:07:37 +0100

    Player Health system
    enemy tag
    Player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody2D player;
+    [SerializeField]
+    int playerHealth = 5;
+    Vector3 respawnPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player.GetComponent<Rigidbody2D>();
+
+        respawnPos = player.position;
+
+        ResetPlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHealth <= 0)
+        {
+            ResetPlayer();
+            playerHealth = 5;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            playerHealth += -1;
+        }
+    }
+    private void ResetPlayer()
+    {
+        transform.position = respawnPos;
+    }
+}
