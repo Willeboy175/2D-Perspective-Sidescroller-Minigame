35d76abb58e04b721b195271570f8379e1c7838a
BoBeliPop <120007768+BoBeliPop@users.noreply.github.com>
2022-12-21 10:47:04 +0100

    agag
    hej

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -30,7 +30,7 @@ public class PlayerHealth : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
